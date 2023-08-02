22510279ced874e7245bc6e912790660f60a99e1
Benjamin Sai <Benjamis@microsoft.com>
2023-08-01 17:46:23 -0700

    updated console logging format

--- src/Program.cs
+++ src/Program.cs
@@ -413,7 +413,12 @@ public static class Program
                         break;
                 }
 
-                builder.AddConsole();
+                builder.AddSimpleConsole(options =>
+                {
+                    options.IncludeScopes = true;
+                    options.SingleLine = true;
+                    options.TimestampFormat = "HH:mm:ss.ffff ";
+                });
                 builder.AddOpenTelemetryLogging();
             });
 
